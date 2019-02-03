d5506e8118a1c3c6cfd796c4a21afe447252a1f5
QA <asif.padani@gmail.com>
2019-02-03 15:05:00 -0600

    second test added

--- gitProject/src/gitProject/Git.java
+++ gitProject/src/gitProject/Git.java
@@ -5,7 +5,10 @@ public class Git {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hwllo");
+		
+		System.out.println("Seocnd test");
 
+		
 	}
 
 }
